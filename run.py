4871453568279f8c3f2257a96f9c5310e3b932a9
realhex444 <ayaansanjeev@gmail.com>
2022-03-21 12:04:06 +0530

    added to-do

--- run.py
+++ run.py
@@ -4,3 +4,14 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+"""
+
+separate css data between files, add reference to them in templates
+fix navbar
+finish login logout sys
+make your first database item
+redesign EVERYTHING
+
+"""
